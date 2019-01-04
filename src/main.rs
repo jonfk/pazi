4b72fb3f61a5945f63419c57ce31dbfebfe1fb02
Euan Kemp <euank@euank.com>
2019-01-04 19:55:20 +0000

    main: reduce size of ctrl-c handler unsafe blocks
    The code should be functionally identical, just a little less code
    inside unsafe blocks.

--- src/main.rs
+++ src/main.rs
@@ -521,7 +521,6 @@ fn intercept_ctrl_c() -> Result<(), ()> {
     // process group and make that the foreground process group.
     // That way, when ctrl-c sends a SIGINT, the only process
     // to receive it is Pazi.
-    //
     unsafe fn get_errno() -> *mut libc::c_int {
         #[cfg(target_os = "linux")]
         return libc::__errno_location();
@@ -529,61 +528,73 @@ fn intercept_ctrl_c() -> Result<(), ()> {
         return libc::__error();
     }
 
-    unsafe {
+    let (setpgid_res, errno) = unsafe {
         // Create a new process group with this process in it.
         let setpgid_res = libc::setpgid(0, 0);
         let errno = *get_errno();
-        if setpgid_res != 0 {
-            debug!("Got {} from setpgid with errno {}", setpgid_res, errno);
-            return Err(());
-        }
+        (setpgid_res, errno)
+    };
 
-        // Get the ID of the process group we just made.
-        let pgrp = libc::getpgrp();
+    if setpgid_res != 0 {
+        debug!("Got {} from setpgid with errno {}", setpgid_res, errno);
+        return Err(());
+    }
 
-        // Make this process group the foreground process.
-        // SIGTTOU is sent if this process group isn't already foreground,
-        // so we ignore it during the change.
+    // Get the ID of the process group we just made.
+    let pgrp = unsafe { libc::getpgrp() };
 
-        // New SIGTTOU handler that ignores the signal
-        let ignore_action = libc::sigaction {
-            sa_sigaction: libc::SIG_IGN,
-            sa_mask: std::mem::zeroed(),
-            sa_flags: 0,
-            #[cfg(target_os = "linux")]
-            sa_restorer: None,
-        };
-        // Place to save old SIGTTOU handler
-        let mut old_action = std::mem::zeroed();
+    // Make this process group the foreground process.
+    // SIGTTOU is sent if this process group isn't already foreground,
+    // so we ignore it during the change.
 
-        // Ignore SIGTTOU and save previous action
+    // New SIGTTOU handler that ignores the signal
+    let ignore_action = libc::sigaction {
+        sa_sigaction: libc::SIG_IGN,
+        sa_mask: unsafe { std::mem::zeroed() },
+        sa_flags: 0,
+        #[cfg(target_os = "linux")]
+        sa_restorer: None,
+    };
+    // Place to save old SIGTTOU handler
+    let mut old_action = unsafe { std::mem::zeroed() };
+
+    // Ignore SIGTTOU and save previous action
+    let (sigaction_res, errno) = unsafe {
         let sigaction_res = libc::sigaction(libc::SIGTTOU, &ignore_action, &mut old_action);
         let errno = *get_errno();
-        if sigaction_res != 0 {
-            debug!("Got {} from sigaction with errno {}", sigaction_res, errno);
-            return Err(());
-        }
+        (sigaction_res, errno)
+    };
 
-        // Make our process group the foreground process group
-        // (giving us access to stdin, etc)
+    if sigaction_res != 0 {
+        debug!("Got {} from sigaction with errno {}", sigaction_res, errno);
+        return Err(());
+    }
+
+    // Make our process group the foreground process group
+    // (giving us access to stdin, etc)
+    let (tcsetpgrp_res, errno) = unsafe {
         let tcsetpgrp_res = libc::tcsetpgrp(libc::STDIN_FILENO, pgrp);
         let errno = *get_errno();
+        (tcsetpgrp_res, errno)
+    };
 
-        // Put the old SIGTTOU signal handler back
-        // We try to do this even if tcsetpgrp failed!
+    // Put the old SIGTTOU signal handler back
+    // We try to do this even if tcsetpgrp failed!
+    let (sigaction_res, sigaction_errno) = unsafe {
         let sigaction_res = libc::sigaction(libc::SIGTTOU, &old_action, std::ptr::null_mut());
         let sigaction_errno = *get_errno();
+        (sigaction_res, sigaction_errno)
+    };
 
-        // Handle tcsetpgrp and sigaction errors
-        if tcsetpgrp_res != 0 || sigaction_res != 0 {
-            debug!("Got pgrp {}", pgrp);
-            debug!("Got {} from tcsetpgrp with errno {}", tcsetpgrp_res, errno);
-            debug!(
-                "Got {} from sigaction with errno {}",
-                sigaction_res, sigaction_errno
-            );
-            return Err(());
-        }
+    // Handle tcsetpgrp and sigaction errors
+    if tcsetpgrp_res != 0 || sigaction_res != 0 {
+        debug!("Got pgrp {}", pgrp);
+        debug!("Got {} from tcsetpgrp with errno {}", tcsetpgrp_res, errno);
+        debug!(
+            "Got {} from sigaction with errno {}",
+            sigaction_res, sigaction_errno
+        );
+        return Err(());
     }
 
     Ok(())
